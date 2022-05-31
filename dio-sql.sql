b57b167c3848b26ba18ea82cec161a3308f54b07
Leozin777 <leo.cardoso@rede.ulbra.br>
2022-05-31 18:36:15 -0300

    banco-de-dados-relacional2

--- dio-sql.sql
+++ dio-sql.sql
@@ -19,3 +19,28 @@ UPDATE pessoa SET genero="M" WHERE id=3;/*add genero*/
 UPDATE pessoa SET genero="M" WHERE id=4;
 UPDATE pessoa SET genero="M" WHERE id=6;
 SELECT COUNT(id), genero FROM pessoa GROUP BY genero;/*conta quantos generos tem dentro da table pessoa*/
+
+
+/*______________________________________________________________________________________________*/
+/*NOVA TABELA:*/
+
+INSERT INTO videos (author, title, likes, dislikes) VALUES('Maria', 'CSS', 10, 2);
+INSERT INTO videos (author, title, likes, dislikes) VALUES('Paulo', 'HTML', 30, 3);
+INSERT INTO videos (author, title, likes, dislikes) VALUES('Maria', 'JavaScript', 25, 5);
+INSERT INTO videos (author, title, likes, dislikes) VALUES('Paulo', 'Python', 40, 10);
+INSERT INTO videos (author, title, likes, dislikes) VALUES('Paulo', 'Python', 30, 5);
+
+
+CREATE TABLE `dio_mysql`.`author` (`id_author` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(30) NOT NULL , `born` DATE NOT NULL , PRIMARY KEY (`id_author`)) ENGINE = InnoDB;
+INSERT INTO author(name, born) VALUES('Maria', '1999-08-21');
+INSERT INTO author(name, born) VALUES('Paulo', '2001-05-23');
+INSERT INTO author(name, born) VALUES('João', '1984-01-06');
+INSERT INTO author(name, born) VALUES('Sergio', '2002-04-20');
+
+UPDATE videos SET author=3 WHERE id_video=1;
+UPDATE videos SET author=3 WHERE id_video=2;
+UPDATE videos SET author=3 WHERE id_video=3;
+UPDATE videos SET author=1 WHERE id_video=4;
+UPDATE videos SET author=2 WHERE id_video=5;
+
+SELECT * FROM videos JOIN author ON videos.fk_author = author.id_author;
